48209eab7030104a7f9d6ab6111b5a5d5f4f7014
star <1598624985@qq.com>
2025-08-13 19:29:35 +0800

    chore: 调整tsconfig和eslint允许any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,6 +23,7 @@ export default tseslint.config(
   {
     files: ['**/*.{ts,mts,tsx,vue}'],
     rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
       'vue/require-default-prop': 'off',
       'vue/multi-word-component-names': 'off',
       'vue/block-lang': [
