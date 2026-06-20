a094f6d8562d4c22b3d7deecd5a3b03dad59f6d0
sqj <sqj@shiqianjiang.cn>
2026-06-20 22:29:39 +0800

    close

--- src/renderer/src/store/index.ts
+++
@@ -1,5 +0,0 @@
-export { searchValue as useSearchStore } from './search' // 搜索
-export { playSetting as usePlaySettingStore } from './playSetting' // 播放设置
-export { useAuthStore } from './Auth' // 认证
-export { useDownloadStore } from './download' // 下载管理
-export { useListenTogetherStore } from './ListenTogether' // 一起听
